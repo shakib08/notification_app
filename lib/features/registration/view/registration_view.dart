753137c5f4a52e6ee5be3387b869939424217131
Rasel <you@example.com>
2024-12-11 15:52:14 +0600

    Problem solved

--- lib/features/registration/view/registration_view.dart
+++ lib/features/registration/view/registration_view.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:notification_app/features/registration/controller/registration_controller.dart';
 
 class RegistrationView extends StatelessWidget {
@@ -118,7 +118,9 @@ class RegistrationView extends StatelessWidget {
                         ),
                         onPressed: () {
                           controller.registerUser();
-                          print('Gradient Button Pressed');
+                          if (kDebugMode) {
+                            print('Gradient Button Pressed');
+                          }
                         },
                         child: const Text(
                           'Register',
